1aa8e3547cf956ceb8cab44d44ef572a4a78b93a
Snoxtar <s20497@pjwstk.edu.pl>
2021-10-16 13:52:06 +0200

    Added null sandwich endpoint

--- src/main/java/pjatk/way/sandwiches/controller/SandwichRestController.java
+++ src/main/java/pjatk/way/sandwiches/controller/SandwichRestController.java
@@ -1,6 +1,7 @@
 package pjatk.way.sandwiches.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pjatk.way.sandwiches.enums.SandwichSize;
@@ -13,10 +14,20 @@ import java.util.List;
 @RequestMapping("/sandwich")
 public class SandwichRestController {
 
+    @GetMapping("/example")
     public ResponseEntity<SandwichModel> getExampleSandwich(){
         Ingredients bread = new Ingredients(null, "white bread", 100, 1.5);
         SandwichModel poor = new SandwichModel(null, "poor", 100, 5.0d, List.of(bread), SandwichSize.KING_SIZE);
 
         return ResponseEntity.ok(poor);
+    }
+
+    @GetMapping("/null/sandwich")
+    public ResponseEntity<SandwichModel> getNullSandwich()
+    {
+        Ingredients nullBread = new Ingredients(null, null, null, null);
+        SandwichModel nullSandwich = new SandwichModel(null, "nullSandwich", 100, 5.0d, null, SandwichSize.KING_SIZE);
+
+        return ResponseEntity.ok(nullSandwich);
     }
 }
