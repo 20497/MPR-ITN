c0a428250a795c9194f891969ea8750337448a02
Snoxtar <s20497@pjwstk.edu.pl>
2021-10-16 12:14:18 +0200

    Initialized spring web project

---
+++ src/test/java/pjatk/way/sandwiches/SandwichesApplicationTests.java
@@ -0,0 +1,13 @@
+package pjatk.way.sandwiches;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandwichesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
